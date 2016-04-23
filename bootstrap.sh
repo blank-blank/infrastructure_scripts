56e7aed081f4f0dfeff24f1aa106591fb29ac577
EC2 Default User <ec2-user@ip-172-31-30-47.us-west-2.compute.internal>
2016-04-23 23:35:04 +0000

    install ansible

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,39 @@
-#==============================================================================
+#!/usr/bin/env bash
+
+#=========================================================================
 # set up new server
-#==============================================================================
-yum install git
+#=========================================================================
 
 
 
+function isinstalled {
+  #usage:if isinstalled $package; then echo "installed"; else echo "not installed"; f
+    
+if yum list installed "$@" >/dev/null 2>&1; then
+    true
+  else
+    false
+  fi
+}
+
+function install {
+    #thin wrapper around yum install - if package already exists, 
+    #does nothing, else installs the package
+    package=$1
+    if isinstalled $package; 
+    then 
+        echo "$package already installed: doing nothing"; 
+    else 
+        echo "$package is not installed"
+        echo "installing $package"
+
+        yum install $package
+        echo "$package now installed"
+        
+    fi
+}
+
+
+
+pip install ansible
 
