3af4886fcf27944fee6e0d403a7c7690049a3669
Tim Lewis <timlewis@Lindys-MacBook-Air.local>
2016-06-23 01:04:39 -0600

    test create stack

---
+++ cloudformation/launch_cf_stack.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -z "$TEMPLATE_DIR" ]
+then
+    TEMPLATE_DIR="$(pwd)/templates"
+fi
+TEMPLATE_FILE="$1"
+TEMPLATE_FILEPATH="${TEMPLATE_DIR}/${TEMPLATE_FILE}"
+
+if [ -z $TEMPLATE_FILE ]
+then
+    echo "Usage: launch_cf_template template_file"
+    exit 1
+fi
+
+if [ ! -f $TEMPLATE_FILEPATH ]
+then
+    echo "TEMPLATE_FILEPATH does not exist" 
+    exit 1
+fi
+exit
+aws cloudformation create-stack --region us-west-2 --template-body file:////templates//mytemplate.json
